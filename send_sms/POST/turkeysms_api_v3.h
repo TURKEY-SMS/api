f04b3726695860f93f62aae05fd46060925474be
TURKEYSMS BİLİŞİM VE İLETİŞİM HİZMETLERİ <68009768+turkeysms@users.noreply.github.com>
2021-09-09 15:15:06 +0300

    Update turkeysms_api_v3.h

--- send_sms/POST/turkeysms_api_v3.h
+++ send_sms/POST/turkeysms_api_v3.h
@@ -3,8 +3,7 @@
 dispatch_semaphore_t sema = dispatch_semaphore_create(0);
 
 NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"https://www.turkeysms.com.tr/api/v3/gonder/add-content"]
-                                                       cachePolicy:NSURLRequestUseProtocolCachePolicy
-                                                   timeoutInterval:10.0];
+cachePolicy:NSURLRequestUseProtocolCachePolicytimeoutInterval:10.0];
 NSDictionary *headers = @{
     @"Content-Type" : @"application/json"
 };
@@ -16,20 +15,19 @@ NSData *postData = [[NSData alloc] initWithData:[@"{\"api_key\":\"API_KEY_HERE\"
 [request setHTTPMethod:@"POST"];
 
 NSURLSession *session = [NSURLSession sharedSession];
-NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request
-                                            completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-                                              if (error)
-                                              {
-                                                  NSLog(@"%@", error);
-                                              }
-                                              else
-                                              {
-                                                  NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
-                                                  NSError *parseError = nil;
-                                                  NSDictionary *responseDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:&parseError];
-                                                  NSLog(@"%@", responseDictionary);
-                                                  dispatch_semaphore_signal(sema);
-                                              }
-                                            }];
+NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+      if (error)
+      {
+          NSLog(@"%@", error);
+      }
+      else
+      {
+          NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+          NSError *parseError = nil;
+          NSDictionary *responseDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:&parseError];
+          NSLog(@"%@", responseDictionary);
+          dispatch_semaphore_signal(sema);
+      }
+    }];
 [dataTask resume];
 dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
